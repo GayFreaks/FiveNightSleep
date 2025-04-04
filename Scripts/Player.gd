da39b69c788080ddf8ae81b7fc768b383feefed8
cornelius <macrosedk@gmail.com>
2025-04-04 23:51:34 +0200

    Blableb

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,6 +6,7 @@ onready var direction_object = $Direction
 var speed = 500  # speed in pixels/sec
 var velocity = Vector2.ZERO
 var mouse_pos_rel_player
+var mouse_rot_rel_player
 
 func get_input():
 	var movement_direction = Vector2(Input.get_axis("Move_Left", "Move_Right"), Input.get_axis("Move_Up", "Move_Down"))
@@ -14,9 +15,17 @@ func get_input():
 
 func _unhandled_input(event):
 	if event is InputEventMouse:
-		mouse_pos_rel_player = (get_viewport().get_mouse_position()-get_global_transform_with_canvas().get_origin())
+		var player_on_screen = get_global_transform_with_canvas().get_origin()
+		var mouse_on_screen = get_viewport().get_mouse_position()
+		mouse_pos_rel_player = mouse_on_screen-player_on_screen
 		mouse_pos_rel_player = mouse_pos_rel_player.clamped(100)
+		mouse_rot_rel_player = mouse_on_screen.angle_to_point(player_on_screen)
 		direction_object.position = mouse_pos_rel_player
+		direction_object.rotation = mouse_rot_rel_player
+
+		if event is InputEventMouseButton:
+			if event.button_index == 1 && event.pressed:
+				print("uwu")
 
 func _physics_process(_delta):
 	velocity = Vector2.ZERO
