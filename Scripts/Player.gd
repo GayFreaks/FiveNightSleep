db91de93f8a3ba487917fa16082145d45efe4fdd
cornelius <macrosedk@gmail.com>
2025-04-04 22:38:48 +0200

    Blablaba

---
+++ Scripts/Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var speed = 200  # speed in pixels/sec
+var velocity = Vector2.ZERO
+
+func get_input():
+	var direction = Vector2(Input.get_axis("Move_Left", "Move_Right"), Input.get_axis("Move_Up", "Move_Down"))
+
+	return direction
+
+func _physics_process(_delta):
+	velocity = Vector2.ZERO
+	velocity = get_input().normalized() * speed
+	velocity = move_and_slide(velocity)
