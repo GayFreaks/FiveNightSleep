e72b7c499d441d092ae5055a3d22f43d6672939e
cornelius <macrosedk@gmail.com>
2025-04-04 23:11:52 +0200

    More player stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,22 @@
 extends KinematicBody2D
+class_name Player
+
+onready var direction_object = $Direction
 
 var speed = 500  # speed in pixels/sec
 var velocity = Vector2.ZERO
+var mouse_pos_rel_player
 
 func get_input():
-	var direction = Vector2(Input.get_axis("Move_Left", "Move_Right"), Input.get_axis("Move_Up", "Move_Down"))
+	var movement_direction = Vector2(Input.get_axis("Move_Left", "Move_Right"), Input.get_axis("Move_Up", "Move_Down"))
 
-	return direction
+	return movement_direction
+
+func _unhandled_input(event):
+	if event is InputEventMouse:
+		mouse_pos_rel_player = (get_viewport().get_mouse_position()-get_global_transform_with_canvas().get_origin())
+		mouse_pos_rel_player = mouse_pos_rel_player.clamped(100)
+		direction_object.position = mouse_pos_rel_player
 
 func _physics_process(_delta):
 	velocity = Vector2.ZERO
