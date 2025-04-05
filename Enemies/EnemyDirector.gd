234911a9d2d3b032d0bf05d4224a928bee14871c
cornelius <macrosedk@gmail.com>
2025-04-05 18:54:12 +0200

    Random location offsets for cats.

--- Enemies/EnemyDirector.gd
+++ Enemies/EnemyDirector.gd
@@ -9,7 +9,7 @@ var current_player = null
 func _ready():
 	var timer := Timer.new()
 	add_child(timer)
-	timer.wait_time = 0.5
+	timer.wait_time = 5
 	timer.connect("timeout", self, "enemy_calc")
 	timer.start()
 
@@ -21,33 +21,5 @@ func enemy_died(enemy):
 				current_door.change_lock(false)
 
 func enemy_calc():
-	match current_enemies.size():
-		1:
-			current_enemies[0].goal = Cat.goals.CHARGE
-		2:
-			current_enemies[0].goal = Cat.goals.CHARGE
-			current_enemies[1].goal = Cat.goals.STALK
-		3:
-			current_enemies[0].goal = Cat.goals.STALK
-			current_enemies[1].goal = Cat.goals.JUMP
-			current_enemies[2].goal = Cat.goals.STALK
-		4:
-			current_enemies[0].goal = Cat.goals.CHARGE
-			current_enemies[1].goal = Cat.goals.STALK
-			current_enemies[2].goal = Cat.goals.JUMP
-			current_enemies[3].goal = Cat.goals.STALK
-		5:
-			current_enemies[0].goal = Cat.goals.CHARGE
-			current_enemies[1].goal = Cat.goals.STALK
-			current_enemies[2].goal = Cat.goals.JUMP
-			current_enemies[3].goal = Cat.goals.STALK
-			current_enemies[4].goal = Cat.goals.STALK
-		6:
-			current_enemies[0].goal = Cat.goals.JUMP
-			current_enemies[1].goal = Cat.goals.JUMP
-			current_enemies[2].goal = Cat.goals.STALK
-			current_enemies[3].goal = Cat.goals.STALK
-			current_enemies[4].goal = Cat.goals.STALK
-			current_enemies[5].goal = Cat.goals.STALK
-		_:
-			pass
+	for i in current_enemies:
+		i.recalc_target()
--- Scripts/Cat.gd
+++ Scripts/Cat.gd
@@ -3,44 +3,20 @@ class_name Cat
 
 export var health = 100
 export var speed = 100
+export var given_damage = 50
 
 onready var enemy_director = get_node("/root/EnemyDirector")
 onready var health_bar = $Control/HealthBar
 
 var rng = RandomNumberGenerator.new()
 
-enum states {
-	IDLE,
-	CHARGE,
-	STALK,
-	JUMP,
-	ATTACK
-}
-
-enum goals {
-	NONE,
-	CHARGE,
-	STALK,
-	JUMP
-}
-
-var state = states.IDLE
-var goal = states.IDLE setget goal_change
 var target_location = Vector2.ZERO
 
-func goal_change(new_goal):
-	if goal == goals.NONE:
-		if new_goal == goals.CHARGE:
-			target_location = enemy_director.current_player.position
-		elif new_goal == goals.STALK:	
-			rng.randomize()
-			var random_degree = rng.randf_range(0.0, 360.0)
-			var random_point = Vector2(cos(deg2rad(random_degree)),sin(deg2rad(random_degree))) * 400
-			target_location = enemy_director.current_player.position + random_point
-
-
-
-	goal = new_goal
+func recalc_target():
+	rng.randomize()
+	var random_degree = rng.randf_range(0.0, 360.0)
+	var random_point = Vector2(cos(deg2rad(random_degree)),sin(deg2rad(random_degree))) * 400
+	target_location = enemy_director.current_player.position + random_point
 
 func _ready():
 	enemy_director.current_enemies.append(self)
@@ -66,16 +42,6 @@ func damage(amount, knockback):
 func _physics_process(_delta):
 	apply_central_impulse((target_location - position).normalized() * speed)
 
-	# if position.distance_to(target_location) < 50:
-	# 	if goal == goals.CHARGE:
-	# 		goal = goals.NONE
-	# 	elif goal == goals.STALK:
-	# 		goal = goals.NONE
-
 func _on_AttackDetect_body_entered(body:Node):
 	if body.is_in_group("Player"):
-		state = states.ATTACK
-		if goal == goals.CHARGE:
-			goal = goals.NONE
-		elif goal == goals.STALK:
-			goal = goals.NONE
+		body.damage(50)
