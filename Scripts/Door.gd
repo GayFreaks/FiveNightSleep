0e2bf86df9c4d174c0fd3e135a9e936983d6feaa
cornelius <macrosedk@gmail.com>
2025-04-05 15:07:23 +0200

    Locked doors

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,8 +1,28 @@
 extends Area2D
 
 onready var loader = get_node("/root/Loading")
+onready var lock_sprite = $Lock
+onready var lock_collision = $StaticBody2D/CollisionShape2D
 
+export var locked = true
 export var level_name = "LevelX"
+
+export(Texture) var lock_icon
+export(Texture) var unlock_icon
+
+func _ready():
+	change_lock(locked)
+
+func change_lock(new_status):
+	locked = new_status
+	if locked:
+		if lock_icon != null:
+			lock_sprite.texture = lock_icon
+		lock_collision.disabled = false
+	else:
+		if unlock_icon != null:
+			lock_sprite.texture = unlock_icon
+		lock_collision.disabled = true
 
 func _on_Door_body_entered(body:Node):
 	if body.is_in_group("Player"):
