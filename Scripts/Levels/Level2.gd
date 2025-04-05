c246fe8ea61d2bed16fc1bed7238af36e8a7f21a
cornelius <macrosedk@gmail.com>
2025-04-05 15:39:51 +0200

    Move door enemy code to doors

--- Scripts/Levels/Level2.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export(NodePath) var door_path
-
-onready var enemy_directory = get_node("/root/EnemyDirector")
-onready var door = get_node(door_path)
-
-func _ready():
-	enemy_directory.current_door = door
